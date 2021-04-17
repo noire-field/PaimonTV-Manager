05ed9b840e84ade12b6fe6d34ae4c66aacbf1dea
Noire Bladefield <noirefield@gmail.com>
2021-04-17 16:44:42 +0700

    My client just called me while I'm working on Episode :(

--- src/routes/Episodes.ts
+++ src/routes/Episodes.ts
@@ -27,44 +27,55 @@ router.get('/:movieId/episodes', CurrentUser, RequireAuth, RequireMovie, async (
     });
 });
 
-/*
-// Add a movie
-router.post('/', [
+// Add an episode
+router.post('/:movieId/episodes', [
+    body('id').isNumeric().isLength({ min: 1, max: 999 }).withMessage('Id must be from 1 to 999'),
     body('title').isLength({ min: 1, max: 64 }).withMessage('Title must be from 1 to 64 characters'),
-    body('subTitle').isLength({ min: 0, max: 64 }).withMessage('Sub title must be from 0 to 64 characters').default(''),
-    body('thumbnail').isURL().withMessage('Thumbnail must be an image url'),
-    body('year').isLength({ min: 1, max: 32}).withMessage('Year must be from 1 to 32 characters')
+    body('duration').isNumeric().isLength({ min: 0, max: 99999 }).withMessage('Duration must be a number in second').default(0),
+    body('url').isURL().withMessage('Url must be valid'),
+    body('status').isNumeric().isIn([0, 2]).withMessage('Status is invalid')
 ],
-CurrentUser, RequireAuth, ValidateRequest, 
+CurrentUser, RequireAuth, RequireMovie, ValidateRequest, 
 async (req: Request, res: Response) => {
-    const { title, subTitle, thumbnail, year } = req.body;
-    const movieId = uuidv4();
+    const { movieData: rawMovieData, movieRef } = req;
+    const { id, title, duration, url, status } = req.body;
 
-    const movieData = {
-        title, 
-        subTitle, 
-        thumbnail, 
-        year
-    };
+    var movieData = rawMovieData.val();
+    var episodes = {};
+
+    if(movieData.episodes && Object.keys(movieData.episodes).length > 0) {
+        episodes = movieData.episodes;
+    }
     
-    try {
-        var userFirebaseRef = req.userFirebaseRef;
+    // Find if the episode is already there?
+    
+    // Add it to episode list
+    const episodeData = { 
+        title,
+        duration,
+        progress: 0,
+        url,
+        status
+    };
 
-        await userFirebaseRef.child('movies').child(movieId).set({
-            ...movieData,
-            createdAt: new Date().getTime()
+    const epId = `ep${id}`;
+
+    try {
+        await movieRef!.child('episodes').child(epId).set({
+            ...episodeData
         });
     } catch(e) {
         console.log(e);
-        throw new InternalServerError('Unable to create a movie (Firebase)');
+        throw new InternalServerError('Unable to add episode (Firebase)');
     }
 
     return res.status(201).send({
         success: true,
-        movie: { id: movieId, ...movieData }
+        movie: { id: epId, ...episodeData }
     });
 });
 
+/*
 // Update
 router.put('/:id', [
     body('title').isLength({ min: 1, max: 64 }).withMessage('Title must be from 1 to 64 characters'),
