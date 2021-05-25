93b7f75e10f776d4e7cc63b5dfa90f20fdb1d1d9
Noire Bladefield <noirefield@gmail.com>
2021-05-25 16:30:36 +0700

    [Client] Watch: Added Slider

--- client/src/components/WatchScreen/VideoControl.jsx
+++ client/src/components/WatchScreen/VideoControl.jsx
@@ -1,22 +1,72 @@
 import React from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import Slider from '@material-ui/core/Slider';
 import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 
+
 function VideoPlayer(props) {
     return (
-        <ControlWrapper>
-            ABC
-        </ControlWrapper>
+        <Container>
+            <Wrapper>
+                <VideoSlider/>
+                <Controls>
+                    <div>
+                        ANIME NAME
+                    </div>
+                    <PlayPauseButton className="fas fa-play"></PlayPauseButton>
+                    <div>
+                        PASS TIME
+                    </div>
+                </Controls>
+            </Wrapper>
+        </Container>
     )
 }
 
-const ControlWrapper = styled.div`
+const VideoSlider = withStyles({
+    root: {
+        color: 'red'
+    },
+    thumb: {
+        transform: 'scale(1.5)'
+    },
+    rail: {
+        transform: 'scaleY(2)'
+    }
+})(Slider);
+
+const Container = styled.div`
     position: absolute;
     bottom: 0;
     left: 0;
     width: 100%;
     height: 15%;
-    background: linear-gradient(to top, rgba(0,0,0,0.5), transparent);
+    color: white;
 `;
+
+const Wrapper = styled.div`
+    padding: 5px 20px;
+    background: linear-gradient(to top, rgba(0,0,0,0.5), transparent);
+    width: 100%;
+    height: 100%
+`;
+
+const Controls = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const PlayPauseButton = styled.i`
+    margin-top: 10px;
+    transition: .2s all;
+    font-size: 2em;
+    color: white;
+
+    :hover {
+        transform: scale(1.1);
+    }
+`
 
 export default React.memo(VideoPlayer);
--- client/src/components/WatchScreen/VideoPlayer.jsx
+++ client/src/components/WatchScreen/VideoPlayer.jsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 function VideoPlayer(props) {
     return (
         <ReactPlayer 
-            width='100%' height='100%' playing={true} muted={true} controls={true}
+            width='100%' height='100%' playing={true} muted={true}
             url='https://storage.googleapis.com/paimontv/users/bm9pcmVmaWVsZEBnbWFpbC5jb20=/86edfc18-6e13-4f40-a11f-8eab72a25303'
         />
     )
