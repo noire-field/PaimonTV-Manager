33e87b475dd2593b0dd22a3bf345d873abe1c9f7
Noire Bladefield <noirefield@gmail.com>
2021-06-11 16:34:54 +0700

    [Client] Added 'Set Custom Url' in Edit Movie page

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const messages = [
+    //'Submitting feedback to Paimon Network...',
+    'Paimon đang dọn nhà...',
+    //'Keqing là nhỏ tsundere...',
+    //'Ganyu ngực bự...'
+]
+
+function GetRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
 function Loading(props) {
     return (
         <div className="loading-screen">
@@ -9,7 +20,7 @@ function Loading(props) {
                         <i className="fas fa-heart icon-front"></i>
                         <i className="fas fa-heart icon-back"></i>   
                     </div>
-                    <p className="m-0">Submitting feedback to Paimon Network...</p>
+                    <p className="m-0">{GetRandomMessage()}</p>
                 </div>
             </div>
         </div>
